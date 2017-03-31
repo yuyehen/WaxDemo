c76a74a96121076b1341a3d4c9ca7e6be192de69
yuyehen <wei.sunw@ymm56.com>
2017-03-31 10:17:15 +0800

    Initial Commit

---
+++ SWWaxDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SWWaxDemo
+//
+//  Created by sunwei on 2017/3/31.
+//  Copyright © 2017年 sunwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SWWaxDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SWWaxDemo
+//
+//  Created by sunwei on 2017/3/31.
+//  Copyright © 2017年 sunwei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
